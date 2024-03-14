8c70f58d883bfa979e43995bdba57ddc46a9a0d0
Aniket7385 <aniketsalave@gmail.com>
2024-03-13 23:40:54 -0700

    removed two lines

--- src/com/allainz/basic/medibuddy.java
+++ src/com/allainz/basic/medibuddy.java
@@ -1,6 +1,7 @@
 package com.allainz.basic;
 import java.time.Duration;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,7 +13,6 @@ public class medibuddy {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.medibuddy.in/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		
 		
 		
 		
